74ed1210853c8411a258d1b8925b6ce8641fa85c
ams77 <ams777go@naver.com>
2021-08-03 16:38:47 +0900

    gogo

--- app/src/main/java/com/example/studygage/gps_Activity.java
+++ app/src/main/java/com/example/studygage/gps_Activity.java
@@ -2,7 +2,15 @@ package com.example.studygage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class gps_Activity extends AppCompatActivity {
 
@@ -10,5 +18,21 @@ public class gps_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps_);
+
+        Button GOTOGPS = (Button) findViewById(R.id.GOTOgps);
+        GOTOGPS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        Button GOTOSIGNUP = (Button) findViewById(R.id.GOTOsignup);
+        GOTOSIGNUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUp_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
