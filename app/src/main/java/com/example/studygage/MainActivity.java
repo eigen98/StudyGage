348dfdffbee3a76cd0199a469779bfeb40195e2e
eigen98 <terrantrace@gmail.com>
2021-08-03 18:17:02 +0900

    0803 1816 commit

--- app/src/main/java/com/example/studygage/MainActivity.java
+++ app/src/main/java/com/example/studygage/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.studygage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button buttonOfList = (Button) findViewById(R.id.GoToList);
+
+        buttonOfList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StudyListFragment studylistfragment = new StudyListFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, studylistfragment);
+                fragmentTransaction.commit();
+
+            }
+        });
+
 
 
 
