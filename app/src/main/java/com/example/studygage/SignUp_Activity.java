34c0301a6371bf5e8e65a555ace994c7ee618e36
eigen98 <terrantrace@gmail.com>
2021-08-26 11:33:41 +0900

    0803 1816 commit

--- app/src/main/java/com/example/studygage/SignUp_Activity.java
+++ app/src/main/java/com/example/studygage/SignUp_Activity.java
@@ -2,7 +2,10 @@ package com.example.studygage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp_Activity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class SignUp_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_);
+
+
+        Button checkButton = (Button) findViewById(R.id.SignUp_Check_Button);
+        checkButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
+
 }
--- app/src/main/java/com/example/studygage/StudyListFragment.java
+++ app/src/main/java/com/example/studygage/StudyListFragment.java
@@ -1,5 +1,6 @@
 package com.example.studygage;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +11,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class StudyListFragment extends Fragment {
-    @Nullable
 
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
@@ -19,5 +31,36 @@ public class StudyListFragment extends Fragment {
 
 
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 }
